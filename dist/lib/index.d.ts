695178b296c878a40f5211810c7c3d71e6c5ced2
Aneurin Barker Snook <a@aneur.in>
2021-10-11 11:40:55 +0100

    chore: generate dist

--- dist/lib/index.d.ts
+++ dist/lib/index.d.ts
@@ -1,2 +1,37 @@
-declare const _default: {};
-export default _default;
+export declare type ListResponse<T> = {
+    results: T[];
+    metadata: {
+        totalCount: number;
+        count: number;
+        page: number;
+        limit: number;
+        skip: number;
+    };
+};
+export declare type TxData = {
+    memo?: string;
+};
+declare type Tx = {
+    timestamp: number;
+    sender: string;
+    recipient: string;
+    amount: number;
+    data: TxData;
+    nonce: number;
+    signature: string;
+    hash: string;
+    block: {
+        height: number;
+        hash: string;
+    };
+    confirmations: number;
+};
+declare type TxsParams = {
+    above?: number;
+    since?: number;
+    page?: number;
+    limit?: number;
+};
+export declare const transaction: (host: string, hash: string) => Promise<Tx>;
+export declare const transactions: (host: string, address?: string | undefined, params?: TxsParams | undefined) => Promise<ListResponse<Tx>>;
+export {};
